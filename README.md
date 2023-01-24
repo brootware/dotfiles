c24ce3e11b784dcdc780b438391c2e62d3c70a2d
Bruce <brootware@outlook.com>
2023-01-24 08:08:20 +0800

    Finalized installation script and added more instructions

--- README.md
+++ README.md
@@ -9,11 +9,37 @@ Clone the repo.
 git clone https://github.com/brootware/dotfiles.git && cd dotfiles
 ```
 
-Run the installation script. The install script will backup all your dotfiles from `$HOME` directory to `$HOME/originaldotfiles/` folder in case something goes wrong and install all the configurations.
+Run the installation script.
 
 ```zsh
 chmod +x installdotfiles.sh
 ./installdotfiles.sh
+```
+
+Help menu will be shown as below.
+
+```zsh
+Usage: show_help [-b] [-i] [-c] [-h]
+  -b   Backup original dotfiles
+  -i   Install dotfiles
+  -c   Backup current config
+  -h   Show this help menu
+```
+
+### To back up your original dotfiles
+
+This will back up your original dotfiles in `originaldotfiles/` folder in your home directory.
+
+```zsh
+./installdotfiles.sh -b
+```
+
+### To install the dotfiles
+
+This will install the dotfiles from this repo to your `$HOME/` directory.
+
+```zsh
+./installdotfiles.sh -i
 ```
 
 This has been tested on wsl2 running on windows 10 and should work on Mac too.
--- installdotfiles.sh
+++ installdotfiles.sh
@@ -1,20 +1,45 @@
 #!/bin/zsh
 
-function backuporiginal {
+function backup_original {
+    rm -rf $HOME/orignaldotfiles
     mkdir $HOME/orignaldotfiles
     cp -a $HOME/.* $HOME/orignaldotfiles/.
+    echo "Current dotfiles have been backed up to originaldotfiles/ folder in home directory."
 }
 
-function installdotfiles {
+function install_dotfiles {
     cp -a dotfilestoinstall/. $HOME/
+    source ~/.zshrc
+    echo "The dotfiles have been installed."
 }
 
-function backupcurrentconfig {
+function backup_current_config {
     cp -a $HOME/.zshrc dotfilestoinstall/.
     cp -a $HOME/.bashrc dotfilestoinstall/.
+    cp -a $HOME/.aliases dotfilestoinstall/.
+    cp -a $HOME/.gitconfig dotfilestoinstall/.
+    echo "Current dotfiles saved as new configurations."
 }
 
-backuporiginal
-installdotfiles
+function show_help {
+    echo "Usage: $0 [-b] [-i] [-c] [-h]"
+    echo "  -b   Backup original dotfiles"
+    echo "  -i   Install dotfiles"
+    echo "  -c   Save current dotfiles as new configurations"
+    echo "  -h   Show this help menu"
+}
 
-# TODO add argument such that you can invoke to backup current config
+if [ $# -eq 0 ]; then
+    show_help
+    exit 0
+fi
+
+while getopts ":bich" opt; do
+  case $opt in
+    b) backup_original ;;
+    i) install_dotfiles ;;
+    c) backup_current_config ;;
+    h) show_help; exit 0;;
+    \?) echo "Invalid option -$OPTARG" ;;
+  esac
+done
