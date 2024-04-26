bba6aedebcee889db904f13cd56f6081deb1a85c
Oaker Min <7734956+brootware@users.noreply.github.com>
2024-04-26 10:24:39 +0700

    changed aliases

--- powershelldot/Microsoft.Powershell_profile.ps1
+++ powershelldot/Microsoft.Powershell_profile.ps1
@@ -2,7 +2,10 @@ function getpubip {(Invoke-WebRequest -uri "http://ifconfig.me/ip").Content}
 Set-Alias pubip getpubip
 
 function CD32 {set-location c:\windows\system32}
-Set-Alias go cd32
+Set-Alias go32 cd32
 
 function edithost {code c:\windows\system32\drivers\etc\hosts}
 Set-Alias host edithost
+
+function getrouterip {(Get-NetRoute | Where-Object { $_.DestinationPrefix -eq '0.0.0.0/0' }).NextHop}
+Set-Alias routerip getrouterip
